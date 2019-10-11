b0d073a6ee9b94c898a79c308ea4dc7a3ffac173
Max Tyulin <max@tyulin.ru>
2019-10-11 15:35:39 +0300

    Update versiontools

--- example_pkg/utils/versiontools.py
+++ example_pkg/utils/versiontools.py
@@ -1,38 +1,27 @@
 """Version tools set."""
 
 import os
-from typing import Callable, Optional, Union
 
-from setuptools_scm import get_version
-
-
-def get_version_from_scm_tag(
-        *,
-        root: str = '.',
-        relative_to: Optional[str] = None,
-        local_scheme: Union[Callable, str] = 'node-and-date',
-) -> str:
+def get_version_from_scm_tag() -> str:
     """Retrieve the version from SCM tag in Git or Hg."""
     try:
-        return get_version(
-            root=root,
-            relative_to=relative_to,
-            local_scheme=local_scheme,
+        import setuptools_scm  # only available during setup time
+
+        local_scheme = 'node-and-date'
+
+        is_pypi_upload = os.getenv('PYPI_UPLOAD') == 'true'
+        if is_pypi_upload:
+            # Cut local version on upload
+            local_scheme = lambda version: ''
+
+        return setuptools_scm.get_version(
+            root='../..',
+            relative_to=__file__,
+            local_scheme = local_scheme
         )
-    except LookupError:
+    except:
         return 'unknown'
 
 
-def cut_local_version_on_upload(version):
-    """Return empty local version if uploading to PyPI."""
-    is_pypi_upload = os.getenv('PYPI_UPLOAD') == 'true'
-    if is_pypi_upload:
-        return ''
-
-    import setuptools_scm.version  # only available during setup time
-    return setuptools_scm.version.get_local_node_and_date(version)
-
-
 def get_self_version():
-    """Calculate the version of the dist itself."""
-    return get_version_from_scm_tag(local_scheme=cut_local_version_on_upload)
+    pass
